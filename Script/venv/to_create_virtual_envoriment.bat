57a64d1084ab8e47cfb778744729fc7f8445392e
lais-carvalho <95021199+lais-carvalho@users.noreply.github.com>
2024-06-19 10:09:50 +0100

    Add files via upload

---
+++ Script/venv/to_create_virtual_envoriment.bat
@@ -0,0 +1,4 @@
+@echo off
+python -m venv projeto_bioinf
+.\projeto_bioinf\Scripts\activate
+pip install -r requirements.txt
---
+++ Script/venv/virtual_enviroment_created.bat
@@ -0,0 +1,18 @@
+cd C:\Users\Álvaro Magno\Desktop\projeto\bioinf_pro
+
+.\bioinf_pro> python -m venv projeto_bioinf
+
+.\bioinf_pro> projeto_bioinf\Scripts\activate
+
+(projeto_bioinf) .\bioinf_pro> pip install pandas
+
+(projeto_bioinf) .\bioinf_pro> pip install biopython
+
+(projeto_bioinf) .\bioinf_pro> pip install bioservices
+
+(projeto_bioinf) .\bioinf_pro> pip install openpyxl
+
+(projeto_bioinf) .\bioinf_pro> pip freeze > requirements.txt
+
+(projeto_bioinf) .\bioinf_pro> deactivate
+
